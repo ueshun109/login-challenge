b865454fe0e512fe131eb89b24ea9cc696b91722
ueshun109 <ush109rock@gmail.com>
2022-02-06 14:19:15 +0900

    UseCasesモジュールを追加

---
+++ UseCases/Tests/UseCasesTests/UseCasesTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UseCases
+
+final class UseCasesTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UseCases().text, "Hello, World!")
+    }
+}
